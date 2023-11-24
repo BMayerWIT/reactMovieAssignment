6bcd6da89f1cc467fef814c3afb96fdaa8cd518f
BrendanMayer <brendan.mayer1234@gmail.com>
2023-11-24 18:56:35 +0000

    Update Api

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -100,4 +100,22 @@
        throw error
     });
   };
+
+  export const getLatestMovies = (id) => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/${id}/latest?api_key=${process.env.REACT_APP_TMDB_KEY}`
+    ).then((response) => {
+      if (!response.ok) {
+        throw new Error(response.json().message);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      // console.log(data)
+      return data;
+    })
+    .catch((error) => {
+       throw error
+    });
+  }; 
     

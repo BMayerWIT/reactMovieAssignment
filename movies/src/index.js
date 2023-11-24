61fce045d443f3c58400402a7c3b036ad658b365
BrendanMayer <brendan.mayer1234@gmail.com>
2023-11-24 16:43:18 +0000

    Added Top Rated Page

--- movies/src/index.js
+++ movies/src/index.js
@@ -11,6 +11,7 @@ import { ReactQueryDevtools } from 'react-query/devtools';
 import MoviesContextProvider from "./contexts/moviesContext";
 import AddMovieReviewPage from './pages/addMovieReviewPage'
 import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
+import TopRatedMoviesPage from "./pages/topRatedMoviesPage";
 
 
 
@@ -36,6 +37,7 @@ const App = () => {
             <Route path="/reviews/form" element={ <AddMovieReviewPage /> } />
             <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
             <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
+            <Route path="/movies/topRated" element={<TopRatedMoviesPage />} />
             <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
             <Route path="/movies/:id" element={<MoviePage />} />
             <Route path="/" element={<HomePage />} />
---
+++ movies/src/pages/topRatedMoviesPage.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { getTopRatedMovies } from "../api/tmdb-api";
+import PageTemplate from '../components/templateMovieListPage';
+import { useQuery } from 'react-query';
+import Spinner from '../components/spinner';
+import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
+
+const TopRatedMoviesPage = (props) => {
+
+  const {  data, error, isLoading, isError }  = useQuery('topRated', getTopRatedMovies)
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>
+  }  
+  const topRatedMovies = data.results;
+
+  // Redundant, but necessary to avoid app crashing.
+  const favorites = topRatedMovies.filter(m => m.favorite)
+  localStorage.setItem('favorites', JSON.stringify(favorites))
+  const addToFavorites = (movieId) => true 
+
+  return (
+    <PageTemplate
+      title="Top Rated"
+      movies={topRatedMovies}
+      action={(movie) => {
+        return <AddToFavoritesIcon movie={movie} />
+      }}
+    />
+);
+};
+export default TopRatedMoviesPage;
